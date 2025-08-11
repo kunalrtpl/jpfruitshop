bbf186e925e787f3d10c9f913fb64cf221c99b2d
kunal_royalways <kunal@rtpltech.com>
2025-08-11 14:42:22 +0530

    Intial commit

--- config/siteConfig.js
+++ config/siteConfig.js
@@ -1,5 +1,6 @@
 export const siteConfig = {
   appName: process.env.NEXT_PUBLIC_APP_NAME || "J.P. FRUIT SHOP",
+  description: "Fresh and organic fruits delivered to your doorstep.",
   contactNumber: process.env.NEXT_PUBLIC_CONTACT_NUMBER || "+91 1234567890",
   email: process.env.NEXT_PUBLIC_EMAIL || "info@jpfruitshop.com",
   linkedin: process.env.NEXT_PUBLIC_LINKEDIN_LINK || "#",
